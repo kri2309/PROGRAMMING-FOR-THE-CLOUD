115d5e28e060c3a33d99cf881ab32de5de8fc934
kristina <56250919+kri2309@users.noreply.github.com>
2022-02-18 10:30:40 +0100

    backend updated
    can now push and get tokens

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,26 @@
 import Express  from "express";
 import cors from "cors"
+import { v4 as uuid} from "uuid";
 
 const app = Express();
 const PORT = 3001; 
+const SecretToken = uuid();
+let requests = 0; 
 
-app.get("/hello", (req, res) =>{
-    const username = req.query.username;
-    const password = req.query.password;
-    console.log(username + " " + password);
-    if(username == "joe" && password == "123"){
-        res.send("Hello Joe");
+
+app.get("/secret", (req, res) =>{
+    const token = req.query.token;
+    requests++;
+    if(token === SecretToken){
+        res.send({result: 200, requests: requests,message:"This is a secret message"});
     }else{
-        res.send("Invalid credentials");
+        res.send({result: 403, message: "Invalid credentials"});
     }
 });
 
 app.use(cors());
+//85ec3480-d832-494e-bdd8-2f52e083e76a
+
+console.log(SecretToken);
 app.listen(PORT, () =>
     console.log("Server Listening on port: "+PORT))
