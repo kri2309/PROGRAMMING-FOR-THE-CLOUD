cf05ec3901c78bdefb379e7a7def88540de75fa3
kristina <56250919+kri2309@users.noreply.github.com>
2022-04-02 12:02:55 +0200

    storage

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -2,7 +2,7 @@ import Express from "express";
 import multer from "multer";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
-const { Storage } = require("@google-cloud/storage");
+import * as Storage from "@google-cloud/storage";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -32,7 +32,16 @@ let imageUpload = multer({
 upload.route("/").post(imageUpload.single("image"), (req, res) => {
   if (req.file) {
     console.log("File downloaded at: " + req.file.path);
+
     //Upload to google cloud
+
+    const storage = new Storage.Storage({ projectId: "programmingforthecloud-340711", keyFilename: "./key.json",});
+    const bucketname = "programmingforthecloud-340711.appspot.com";
+
+    await storage.bucket(bucketname).upload(req.file.path, {
+    destination: "pending/" + req.file.originalname,
+  });
+
     //Convert to base64
     //Send to PDF Conversion API
     res.send({
@@ -43,19 +52,4 @@ upload.route("/").post(imageUpload.single("image"), (req, res) => {
 });
 
 export default upload;
-
-const storage = new Storage({ keyFilename: './google-cloud-key.json' });
-const bucketname = "programmingforthecloud-340711.appspot.com/pending";
-const filename = 'uploads/test.png';
-const res = await storage.bucket(bucketname).upload('./' + filename);
-// `mediaLink` is the URL for the raw contents of the file.
-const url = res[0].metadata.mediaLink;
-
-// Need to make the file public before you can access it.
-await storage.bucket(bucketname).file(filename).makePublic();
-
-// Make a request to the uploaded URL.
-const axios = require('axios');
-const pkg = await axios.get(url).then(res => res.data);
-pkg.name; // 'masteringjs.io'
 
