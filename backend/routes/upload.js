33ddc6d4876a5139502389f1689bfee284930687
kristina <intern3@stl.com.mt>
2022-04-23 16:38:14 +0200

    Update upload.js

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -91,7 +91,7 @@ upload.route("/").post(imageUpload.single("image"),async function (req, res){
     });
     */
     //const NewName = `completed/`+(path.parse(req.file.originalname).name+".pdf");
-    const NewName = req.file.originalname.replace(path.extname(req.file.originalname));
+    const NewName = req.file.originalname.replace(path.extname(req.file.originalname),"");
     await storage.bucket(bucketname).file(`completed/${NewName}.pdf`).save(newfile);
     
    console.log(NewName);
