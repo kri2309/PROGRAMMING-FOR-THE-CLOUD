2364cc5f70c8d94cc8fe944f37b1999890fd58f0
kristina <intern3@stl.com.mt>
2022-04-23 14:25:48 +0200

    help and pray

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import * as Storage from "@google-cloud/storage";
 import fs from 'fs'
+import {Base64} from 'js-base64';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -45,14 +46,41 @@ upload.route("/").post(imageUpload.single("image"),async function (req, res){
   });
 
     //Convert to base64
-
-    
     var base64file = "data:image/gif;base64," + fs.readFileSync(req.file.path, 'base64');
-    
-
-  console.log(base64file);
+    console.log(base64file);
 
     //Send to PDF Conversion API
+
+    var formData = new FormData();
+    const url = `https://getoutpdf.com/api/convert/image-to-pdf    `;
+    const headers = {
+      "Content-Type": "multipart/form-data",
+      "Access-Control-Allow-Origin": "*",
+    }
+    formData.append("api_key", ed4129c1077bfcfbe13885c696190a477b0ac821e09371b7076b2454cdb35c83);
+    formData.append("image", base64file);
+    formData.append("transparent_color","#ffffff" );
+
+    const response = await axios.post(url, formData, headers);
+    console.log(response.data.pdf_base64);
+    console.log(response.data.tokens_used);
+    console.log(response.data.tokens_left);
+
+    var buf = Buffer.from(stringToDecode, 'base64');
+    // Your code to handle buffer
+    fs.writeFile(path.parse(req.file.originalname).name+".pdf", buf, error => {
+        if (error) {
+            throw error;
+        } else {
+            console.log('buffer saved!');
+        }
+    });
+
+    await storage.bucket(bucketname).upload(buf, {
+        destination: "completed/" + path.parse(req.file.originalname).name+".pdf",
+    });
+
+
     res.send({
       status: "200",
       message: "File uploaded successfully! Processing..",
