84dd344ce1792780f556db3fad7fad3b73e1777c
kristina <56250919+kri2309@users.noreply.github.com>
2022-04-26 17:07:16 +0200

    Update payments.js

--- backend/routes/payments.js
+++ backend/routes/payments.js
@@ -13,15 +13,9 @@ const __dirname = dirname(__filename);
 const payments = Express.Router();
 
 payments.route("/").get((req,res) => {
+
     res.sendFile(path.join(__dirname, "../frontend/index.html"));
-    const email = req.query.email;
-    GetUser(email).then( async function(response){
-        if(response.length > 0){
-          console.log("found user "+response[0].credits );
-          res.send({ result: "exists", reason: "Found email", credits: response[0].credits, admin: response[0].admin});
-    
-        }
-      });
+
     
 });
 
