6755c524cfa7d51ade6f8470c76631ce98597089
kristina <56250919+kri2309@users.noreply.github.com>
2022-04-26 17:17:59 +0200

    Update index.js

--- frontend/public/assets/js/index.js
+++ frontend/public/assets/js/index.js
@@ -129,8 +129,6 @@ async function loadGoogleLogin() {
          credits.innerHTML = "Credits: "+response.data.credits;
          tempcred = response.data.credits;
          sessionStorage.setItem("credits", response.data.credits);
-        console.log("SetItem in session: " + response.data.credits);
-        
         }
        },
       function (error) {
